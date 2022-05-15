4d1d764cbcd5ea5053a67a5e05035eb25571d21c
NastyaSmir <101507367+NastyaSmir@users.noreply.github.com>
2022-05-15 22:33:58 +0200

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,37 @@
 var express = require('express');
 var cors = require('cors');
 var path = require('path');
+const fs = require("fs")
 
 var app = express();
 app.use(cors());
 
-app.get('/events', function(request, response){
+app.get('/api/events', function(request, response){
 	var fileName = path.resolve(__dirname, './data/events.json');
 	response.sendFile(fileName, {})
 });
 
-app.listen(7000)
+app.post('/api/events/create',function (request,response){
+	// TODO: нормально парсить body, чтобы создавать события
+	const event = {
+		id:request.body.id,
+		title:request.body.title,
+		description:request.body.description,
+		date:request.body.date
+	}
+
+	fs.readFile('./data/events.json',function (err,data){
+		const json = JSON.parse(data)
+		json.events.push(event)
+		fs.writeFile("./data/events.json",JSON.stringify(json))
+	})
+
+	response.send(event)
+})
+
+app.post('/api/events/update',function (request,response){
+
+})
+
+app.listen(8080)
 
